3713c339c31f19a6d00ab445f239b47e799a9bc6
Cristobal Medrano <cristobal.medrano@usach.cl>
2021-07-18 22:35:45 -0400

    gitflow-feature-stash: CrossOrigin

--- src/main/java/two/microservice/controller/DiplomateController.java
+++ src/main/java/two/microservice/controller/DiplomateController.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin("*")
 public class DiplomateController {
 
     private final DiplomateRepository diplomateRepository;
